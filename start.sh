1c3200bc93ad7c51478f7ec38389558871e45b13
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 09:29:42 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/NaysaBots/Encdoder-compress-pro-bot /Encdoder-compress-pro-bot
-cd /Encdoder-compress-pro-bot
-pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @Tellybots"
-python3 -m bot
