84303dc6bf76291589755ee3c75bb2a995b52441
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 14:32:11 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -119,7 +119,28 @@ if __name__ == "__main__" :
         await message.download(file_name='/app/thumb.jpg')
         await message.reply_text('Thumbnail Added')
         
-
+    @app.on_callback_query()
+    async def button(bot, update):
+        if update.data == "home":
+            await update.message.edit_text(
+                text=Translation.START_TEXT.format(update.from_user.mention),
+                reply_markup=Translation.START_BUTTONS,
+                disable_web_page_preview=True
+            )
+        elif update.data == "help":
+            await update.message.edit_text(
+                text=Translation.HELP_TEXT,
+                reply_markup=Translation.HELP_BUTTONS,
+                disable_web_page_preview=True
+            )
+        elif update.data == "about":
+            await update.message.edit_text(
+                text=Translation.ABOUT_TEXT,
+                reply_markup=Translation.ABOUT_BUTTONS,
+                disable_web_page_preview=True
+            )
+        else:
+            await update.message.delete()
   
     @app.on_message(filters.incoming & filters.command(["log", f"log@{BOT_USERNAME}"]))
     async def help_message(app, message):
