dedb897d1d322bcf268918a77ea551092e4ac038
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 12:30:27 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -94,7 +94,7 @@ if __name__ == "__main__" :
                
     @app.on_message(filters.incoming & filters.command(["set_res", f"resolution@{BOT_USERNAME}"]))
     async def changer(app, message):
-            text = await message.reply_text(Command.Text)
+            text = await message.reply_text(Command.TEXT)
             r = message.text.split(" ", maxsplit=1)[1]
             OUT = f"I will be using : {r} resolution"
             resolution.insert(0, f"{r}")
