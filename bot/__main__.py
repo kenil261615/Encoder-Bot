e3466f007dee22651e92c6c284136b65ab4dba86
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 17:38:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -154,8 +154,80 @@ if __name__ == "__main__" :
       p = f"🌋Pɪɴɢ = {ms}ms"
       await message.reply_text(v + "\n" + p)
 
-    
+    @app.on_message(filters.command(["compress", "compress@{BOT_USERNAME}"]) & ~ filters.edited)
+    async def compox(s: shakida, message: Message):
+              global temp
+              tempid = uuid.uuid4()
+              video = message.reply_to_message
+       
+              any = message.from_user.id
+         
+          
+              if not video.video:
+                 await s.send_message(message.chat.id, f'**No video provided ‼️')
+                 return
+              else:
+                 f = await s.send_message(message.chat.id, f"**🔄 Prosesing**")
+                 if len(message.command) != 2:
+                    crf = 28
+                 if len(message.command) == 2:
+                    crf = int(message.text.split(None)[1])
+                 if (crf < 20) or (crf > 50):
+                    await f.edit(f'**ERROR!**\nCRF 20-50 value only or default 27')
+                    return
+                 file_n = video.video.file_name
+                 ch = video.video.mime_type.split('/')[1]
+                 duration = video.video.duration
+                 file_s = video.video.file_size
+                 height = video.video.height
+                 width = video.video.width
+                 file = f'{video.video.file_unique_id}.mkv'
+                 butt = InlineKeyboardMarkup([[InlineKeyboardButton("⚙️ Status", callback_data=f"sys"),]])
+                 temp.append(str(file))
+                 await f.edit(f'**🏷️ File Name:** `{file_n}`\n**📥 DOWNLOADING...**\n'
+                 + f'**🍻 CC:** {message.from_user.first_name}', reply_markup=butt)
+                 try:
+                    videox = await video.download(file)
+                 except Exception as e:
+                    temp.pop(0)
+                    await f.edit(f'**ERROR!!: Downloading error.\n`{e}`')
+                    return
 
-    # run the APPlication
+                 try:
+                    but = InlineKeyboardMarkup([[
+                      InlineKeyboardButton("❌ Cancel", callback_data=f'cl {file}|{crf}|{any}'),
+                      InlineKeyboardButton("⚙️ Status", callback_data=f"sys"),
+                      ]])
+                    await f.edit(f'**🏷️ File Name:** ` {file_n}`\n**🗜️ COMPRESSING...**\n**⚙️ CRF Range:** `{crf}`\n'
+                    + f'**🍻 CC:** {message.from_user.first_name}', reply_markup=but)
+                    proc = await asyncio.create_subprocess_shell(
+                      f'ffmpeg -hide_banner -loglevel quiet -i "{videox}" -preset ultrafast -vcodec libx265 -crf {crf} "{file}" -y',
+                      stdout=asyncio.subprocess.PIPE,
+                      stderr=asyncio.subprocess.PIPE,
+                      )
+                    try:
+                       await proc.communicate()
+                    except Exception as e:
+                       await f.edit(f'**ERROR!!:** {e}`')
+                       return
+                    out = f"{file}"
+                    os.remove(videox)
+                    await f.edit(f'**🏷️ File Name:** `{file_n}`\n**COMPRESSION SUCCESSFULLY DONE ✅**\n**📤 File Uploading...**\n'
+                    + f'**🍻 CC:** {message.from_user.first_name}', reply_markup=but)
+                    await video.reply_video(out, duration=duration, height=height, width=width, caption=f'**🏷️ File Name: `{file_n}`'
+                    + f'\n**🚦 Preset:** `Ultrafast`\n**⚙️ CRF:** `{crf}`\n'
+                    + f'**💾 Orginal size:** `{humanbytes(file_s)}`\n'
+                    + f'**🍻 CC:** {message.from_user.mention()}')
+                    os.remove(file)
+                    temp.pop(0)
+                    await f.delete()
+                 except Exception as a:
+                    os.remove(videox)
+                    temp.pop(0)
+                    await f.edit(f'**ERROR!:**\n`{a}`')
+                    return
+
+             
+
     app.run()
 
